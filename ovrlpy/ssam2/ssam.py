76d5c7ff8a6b5e75df1ed9db6ea1a434230d733c
niklasmueboe <sputnik.mb@web.de>
2024-10-17 15:10:27 +0200

    refactor expression sampling

--- ovrlpy/ssam2/ssam.py
+++ ovrlpy/ssam2/ssam.py
@@ -1,5 +1,5 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from typing import Optional
+from typing import Collection, Iterable, Optional
 
 import anndata
 import numpy as np
@@ -15,280 +15,124 @@ def sample_expression(
     kde_bandwidth: float = 2.5,
     minimum_expression: int = 2,
     min_pixel_distance: int = 5,
-    x: Optional[np.ndarray] = None,
-    y: Optional[np.ndarray] = None,
-    z: Optional[np.ndarray] = None,
+    coords: Optional[Collection[np.ndarray]] = None,
     genes: Optional[np.ndarray] = None,
-    x_column: str = "x",
-    y_column: str = "y",
-    z_column: str = "z",
+    coord_columns: Iterable[str] = ["x", "y", "z"],
     gene_column: str = "gene",
     n_workers: int = 8,
-    mode: str = None,
+    mode: Optional[str] = None,
 ) -> anndata.AnnData:
     """
     Sample expression from a coordinate dataframe.
 
-    Parameters:
-    - coordinate_dataframe (Optional[pd.DataFrame]): The input coordinate dataframe.
-    - kde_bandwidth (float): Bandwidth for kernel density estimation.
-    - minimum_expression (int): Minimum expression value for local maxima determination.
-    - min_pixel_distance (int): Minimum pixel distance for local maxima determination.
-    - x (Optional[np.ndarray]): Array of x-coordinates.
-    - y (Optional[np.ndarray]): Array of y-coordinates.
-    - z (Optional[np.ndarray]): Array of z-coordinates.
-    - genes (Optional[np.ndarray]): Array of gene values.
-    - x_column (str): Name of the x-coordinate column in the coordinate dataframe.
-    - y_column (str): Name of the y-coordinate column in the coordinate dataframe.
-    - z_column (str): Name of the z-coordinate column in the coordinate dataframe.
-    - gene_column (str): Name of the gene column in the coordinate dataframe.
-    - n_workers (int): Number of parallel workers for sampling.
-    - mode (str): Sampling mode, either '2d' or '3d'.
+    Parameters
+    ----------
+        coordinate_dataframe : Optional[pandas.DataFrame]
+            The input coordinate dataframe.
+        kde_bandwidth : float
+            Bandwidth for kernel density estimation.
+        minimum_expression : int
+            Minimum expression value for local maxima determination.
+        min_pixel_distance : int
+            Minimum pixel distance for local maxima determination.
+        coords : Collection[numpy.ndarray], optional
+            A list of arrays holding the coordinates.
+        genes : numpy.ndarray, optional
+            Array of gene values.
+        coord_columns : Iterable[str], optional
+            Name of the coordinate columns in the coordinate dataframe.
+        gene_column : str, optional
+            Name of the gene column in the coordinate dataframe.
+        n_workers : int, optional
+            Number of parallel workers for sampling.
+        mode : str, optional
+            Sampling mode, either '2d' or '3d'.
 
-    Returns:
-    - anndata.AnnData: An Anndata object containing sampled expression values.
+    Returns
+    -------
+        anndata.AnnData: An Anndata object containing sampled expression values.
     """
+
+    coord_columns = list(coord_columns)
 
     if mode is None:
         if coordinate_dataframe is not None:
-            if z_column in coordinate_dataframe.columns:
-                mode = "3d"
-            else:
-                mode = "2d"
+            mode = "3d" if coord_columns[-1] in coordinate_dataframe.columns else "2d"
+        elif coords is not None:
+            mode = "3d" if len(coords) == 3 else "2d"
         else:
-            if z is not None:
-                mode = "3d"
-            else:
-                mode = "2d"
+            raise ValueError(
+                "Either `coordinate_dataframe` or `coords` and `genes` must be provided."
+            )
 
     if mode == "2d":
         print("Analyzing in 2d mode:")
-        return _sample_expression_2d(
-            coordinate_dataframe=coordinate_dataframe,
-            kde_bandwidth=kde_bandwidth,
-            minimum_expression=minimum_expression,
-            x=x,
-            y=y,
-            genes=genes,
-            x_column=x_column,
-            y_column=y_column,
-            gene_column=gene_column,
-            n_workers=n_workers,
-        )
+        coord_columns = coord_columns[:2]
+
     elif mode == "3d":
         print("Analyzing in 3d mode:")
-        return _sample_expression_3d(
-            coordinate_dataframe=coordinate_dataframe,
-            kde_bandwidth=kde_bandwidth,
-            minimum_expression=minimum_expression,
-            min_pixel_distance=min_pixel_distance,
-            x=x,
-            y=y,
-            z=z,
-            genes=genes,
-            x_column=x_column,
-            y_column=y_column,
-            z_column=z_column,
-            gene_column=gene_column,
-            n_workers=n_workers,
-        )
+
     else:
         raise ValueError(
             "Could not determine whether to use '2d' or '3d' analysis mode. Please specify mode='2d' or mode='3d'."
         )
 
-
-def _sample_expression_2d(
-    coordinate_dataframe=None,
-    kde_bandwidth=2.5,
-    minimum_expression=2,
-    min_pixel_distance=5,
-    x=None,
-    y=None,
-    genes=None,
-    x_column="x",
-    y_column="y",
-    gene_column="gene",
-    n_workers=8,
-):
-    """
-    Sample expression from a coordinate dataframe.
-    """
-
-    gene_list = sorted(coordinate_dataframe[gene_column].unique())
-
-    if coordinate_dataframe is None:
-        if x is None or y is None or genes is None:
-            raise ValueError(
-                "Either coordinate_dataframe_ or x,y,genes must be provided."
-            )
-        else:
-            coordinate_dataframe_ = pd.DataFrame(
-                {
-                    x_column: x,
-                    y_column: y,
-                    # z_column:z,
-                    gene_column: genes,
-                }
-            )
-
-    else:
-        coordinate_dataframe_ = coordinate_dataframe.copy()
-
-    # lower resolution instead of increasing bandwidth!
-    coordinate_dataframe_[[x_column, y_column]] /= kde_bandwidth / 1.5
-
-    print("determining local maxima:")
-    x_, _x = (
-        int(np.min(coordinate_dataframe_[x_column])),
-        int(np.ceil(np.max(coordinate_dataframe_[x_column]))),
-    )
-    y_, _y = (
-        int(np.min(coordinate_dataframe_[y_column])),
-        int(np.ceil(np.max(coordinate_dataframe_[y_column]))),
-    )
-
-    # perform a global KDE to determine local maxima:
-    vector_field_norm = utils.kde_2d(
-        coordinate_dataframe_[[x_column, y_column]].values, bandwidth=1.5
-    )
-    local_maximum_coordinates = utils.find_local_maxima(
-        vector_field_norm,
+    return _sample_expression_nd(
+        coordinate_dataframe=coordinate_dataframe,
+        kde_bandwidth=kde_bandwidth,
+        minimum_expression=minimum_expression,
         min_pixel_distance=min_pixel_distance,
-        min_expression=minimum_expression,
+        coords=coords,
+        genes=genes,
+        coord_columns=coord_columns,
+        gene_column=gene_column,
+        n_workers=n_workers,
     )
 
-    print("found", len(local_maximum_coordinates), "local maxima")
 
-    size = vector_field_norm.shape
-
-    del vector_field_norm
-
-    # store in anndata object:
-    adata_ssam = anndata.AnnData(
-        var=pd.DataFrame(index=gene_list),
-        obs=pd.DataFrame(index=range(len(local_maximum_coordinates))),
-    )
-
-    adata_ssam.obsm["spatial"] = local_maximum_coordinates * kde_bandwidth
-
-    adata_ssam.uns["ssam"] = {
-        "kde_bandwidth": kde_bandwidth,
-        "x_": x_,
-        "_x": _x,
-        "y_": y_,
-        "_y": _y,
-        "size": size,
-        "coordinates": coordinate_dataframe,
-        "x_column": x_column,
-        "y_column": y_column,
-        "gene_column": gene_column,
-    }
-    adata_ssam.X = np.zeros((len(local_maximum_coordinates), len(gene_list)))
-
-    df_gene_grouped = coordinate_dataframe_.groupby(gene_column, observed=False).apply(
-        lambda x: x[[x_column, y_column]].values
-    )
-
-    df_gene_grouped = df_gene_grouped[df_gene_grouped.apply(lambda x: x.shape[0] > 0)]
-
-    print("sampling expression:")
-
-    with ThreadPoolExecutor(max_workers=n_workers) as executor:
-        fs = {
-            executor.submit(
-                utils.kde_and_sample,
-                df_gene_grouped.loc[gene],
-                local_maximum_coordinates,
-                size=size,
-            ): gene
-            for gene in gene_list
-        }
-        for f in as_completed(fs):
-            gene = fs[f]
-
-            try:
-                output = f.result()
-                adata_ssam.X[:, adata_ssam.var.index == gene] = output[:, None]
-
-            except Exception as exc:
-                print("%r generated an exception: %s" % (gene, exc))
-
-    return adata_ssam
-
-
-def _sample_expression_3d(
+def _sample_expression_nd(
     coordinate_dataframe: Optional[pd.DataFrame] = None,
     kde_bandwidth: float = 2.5,
     minimum_expression: float = 2,
     min_pixel_distance: int = 5,
-    x: Optional[np.ndarray] = None,
-    y: Optional[np.ndarray] = None,
-    z: Optional[np.ndarray] = None,
+    coords: Optional[Iterable[np.ndarray]] = None,
     genes: Optional[np.ndarray] = None,
-    x_column: str = "x",
-    y_column: str = "y",
-    z_column: str = "z",
+    coord_columns: Iterable[str] = ["x", "y", "z"],
     gene_column: str = "gene",
     n_workers: int = 8,
 ) -> anndata.AnnData:
-    """
-    Sample expression from a coordinate dataframe.
-
-    Parameters:
-    - coordinate_dataframe (Optional[pd.DataFrame]): The input coordinate dataframe.
-    - kde_bandwidth (float): Bandwidth for kernel density estimation.
-    - minimum_expression (float): Minimum expression value for local maxima determination.
-    - min_pixel_distance (int): Minimum pixel distance for local maxima determination.
-    - x (Optional[np.ndarray]): Array of x-coordinates.
-    - y (Optional[np.ndarray]): Array of y-coordinates.
-    - z (Optional[np.ndarray]): Array of z-coordinates.
-    - genes (Optional[np.ndarray]): Array of gene values.
-    - x_column (str): Name of the x-coordinate column in the coordinate dataframe.
-    - y_column (str): Name of the y-coordinate column in the coordinate dataframe.
-    - z_column (str): Name of the z-coordinate column in the coordinate dataframe.
-    - gene_column (str): Name of the gene column in the coordinate dataframe.
-
-    Returns:
-    - anndata.AnnData: An Anndata object containing sampled expression values.
-    """
-
-    gene_list = sorted(coordinate_dataframe[gene_column].unique())
+    coord_columns = list(coord_columns)
 
     if coordinate_dataframe is None:
-        if x is None or y is None or z is None or genes is None:
+        if coords is None or genes is None:
             raise ValueError(
-                "Either coordinate_dataframe_ or x,y,z,genes must be provided."
+                "Either `coordinate_dataframe` or `coords` and `genes` must be provided."
             )
         else:
             coordinate_dataframe_ = pd.DataFrame(
-                {x_column: x, y_column: y, z_column: z, gene_column: genes}
+                dict(zip(coord_columns, coords)) | {gene_column: genes}
             )
-
     else:
         coordinate_dataframe_ = coordinate_dataframe.copy()
 
+    gene_list = sorted(coordinate_dataframe_[gene_column].unique())
+
     # lower resolution instead of increasing bandwidth!
-    coordinate_dataframe_[[x_column, y_column, z_column]] /= kde_bandwidth
+    coordinate_dataframe_[coord_columns] /= kde_bandwidth
 
     print("determining local maxima:")
-    x_, _x = (
-        int(np.min(coordinate_dataframe_[x_column])),
-        int(np.ceil(np.max(coordinate_dataframe_[x_column]))),
-    )
-    y_, _y = (
-        int(np.min(coordinate_dataframe_[y_column])),
-        int(np.ceil(np.max(coordinate_dataframe_[y_column]))),
-    )
-    z_, _z = (
-        int(np.min(coordinate_dataframe_[z_column])),
-        int(np.ceil(np.max(coordinate_dataframe_[z_column]))),
-    )
+    bounds = [
+        (
+            int(np.min(coordinate_dataframe_[c])),
+            int(np.ceil(np.max(coordinate_dataframe_[c]))),
+        )
+        for c in coord_columns
+    ]
 
-    print(x_, _x, y_, _y, z_, _z)
+    print(bounds)
     # perform a global KDE to determine local maxima:
-    vector_field_norm = utils.kde_3d(
-        coordinate_dataframe_[[x_column, y_column, z_column]].values, bandwidth=1.1
+    vector_field_norm = utils._kde_nd(
+        coordinate_dataframe_[coord_columns].values, bandwidth=1.1
     )
     local_maximum_coordinates = utils.find_local_maxima(
         vector_field_norm,
@@ -310,25 +154,24 @@ def _sample_expression_3d(
 
     adata_ssam.obsm["spatial"] = local_maximum_coordinates * kde_bandwidth
 
-    adata_ssam.uns["ssam"] = {
+    ssam_params = {
         "kde_bandwidth": kde_bandwidth,
-        "x_": x_,
-        "_x": _x,
-        "y_": y_,
-        "_y": _y,
-        "z_": z_,
-        "_z": _z,
         "size": size,
         "coordinates": coordinate_dataframe,
-        "x_column": x_column,
-        "y_column": y_column,
-        "z_column": z_column,
         "gene_column": gene_column,
     }
+    ssam_params |= {
+        k: v for k, v in zip(["x_column", "y_column", "z_column"], coord_columns)
+    }
+    for (min_i, max_i), name in zip(bounds, ["x", "y", "z"]):
+        ssam_params |= {f"{name}_": min_i, f"_{name}": max_i}
+
+    adata_ssam.uns["ssam"] = ssam_params
+
     adata_ssam.X = np.zeros((len(local_maximum_coordinates), len(gene_list)))
 
     df_gene_grouped = coordinate_dataframe_.groupby(gene_column, observed=False).apply(
-        lambda x: x[[x_column, y_column, z_column]].values
+        lambda x: x[coord_columns].values
     )
 
     df_gene_grouped = df_gene_grouped[df_gene_grouped.apply(lambda x: x.shape[0] > 0)]
@@ -351,7 +194,7 @@ def _sample_expression_3d(
 
                 try:
                     output = f.result()
-                    adata_ssam.X[:, adata_ssam.var.index == gene] = output[:, None]
+                    adata_ssam.X[:, adata_ssam.var_names == gene] = output[:, None]
 
                 except Exception as exc:
                     print("%r generated an exception: %s" % (gene, exc))
