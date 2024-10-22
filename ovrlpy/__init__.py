e6d374c7d6dbe0d67bce8804ee9837f6b28d9ede
tiesmeys <sebastian.tiesmeyer@charite.de>
2024-10-22 13:35:04 +0200

    Fixed package structure, ovrlp functions are now accessible from main module.

--- ovrlpy/__init__.py
+++ ovrlpy/__init__.py
@@ -1,8 +1,33 @@
 from importlib.metadata import PackageNotFoundError, version
 
+from ._ovrlp import (
+    Visualizer,
+    compute_coherence_map,
+    detect_doublets,
+    get_expression_vectors_at_rois,
+    get_rois,
+    load_visualizer,
+    plot_instance,
+    plot_signal_integrity,
+    pre_process_coordinates,
+)
+
 try:
     __version__ = version("ovrlpy")
 except PackageNotFoundError:
     __version__ = "unknown version"
 
 del PackageNotFoundError, version
+
+
+__all__ = [
+    "compute_coherence_map",
+    "detect_doublets",
+    "get_expression_vectors_at_rois",
+    "get_rois",
+    "load_visualizer",
+    "plot_instance",
+    "plot_signal_integrity",
+    "pre_process_coordinates",
+    "Visualizer",
+]
